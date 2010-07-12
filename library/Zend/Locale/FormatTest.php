2fd05c0028b00ae8769980369dc0317bc76f0cae
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-12 17:35:12 +0000

    [ZF-10090] Zend_Filter:
    - merged r22551 to branch

--- library/Zend/Locale/FormatTest.php
+++ library/Zend/Locale/FormatTest.php
@@ -17,7 +17,7 @@
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id$
+ * @version    $Id: FormatTest.php 21492 2010-03-14 12:01:14Z thomas $
  */
 
 require_once dirname(__FILE__) . '/../../TestHelper.php';
