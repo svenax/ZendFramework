4d094fe98cf8c704ae8c3ae6c894728c2f94fd2e
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-10-21 10:23:30 +0000

    Update ZF version.

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.11.0b1';
+    const VERSION = '1.11.0rc1';
 
     /**
      * The latest stable version Zend Framework available
