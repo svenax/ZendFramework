8224df53e89b927a85b7946c359f2c3814704f6d
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-09-29 15:31:19 +0000

    [RELEASE] Updated version strings to 1.11.11

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.11.10';
+    const VERSION = '1.11.11';
 
     /**
      * The latest stable version Zend Framework available
