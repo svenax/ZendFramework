d2b59ab8bdb47859b6082cf1972ff96c90d4f130
digitalstruct <digitalstruct@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-12-05 14:05:12 +0000

    Allowed badge to be null

--- library/Zend/Mobile/Push/Message/Apns.php
+++ library/Zend/Mobile/Push/Message/Apns.php
@@ -137,7 +137,7 @@ class Zend_Mobile_Push_Message_Apns extends Zend_Mobile_Push_Message_Abstract
      */
     public function getBadge()
     {
-        return (int) $this->_badge;
+        return $this->_badge;
     }
 
     /**
